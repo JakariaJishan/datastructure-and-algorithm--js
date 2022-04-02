5813cf57df8e53b943b4d4d9721691890a5f6eff
JakariaJishan <jakariajishan987@gmail.com>
2022-04-02 10:22:37 +0600

    dsa

---
+++ queue.js
@@ -0,0 +1,11 @@
+//first in first out (fifo)
+let queue = [];
+//enqueye
+queue.push('jack');
+queue.push('joe');
+queue.push('jishan');
+queue.push('jacke');
+console.log(queue);
+//dequeue
+queue.shift();
+console.log(queue);
